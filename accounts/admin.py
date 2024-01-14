068abac15491d60ddea5d51a65564bea59c127ca
Koushik <koushik.dey@live.com>
2024-01-14 23:50:06 +0200

    :sparkles: Made password noneditable and added more fields in user admin panel
     Made password noneditable and added more fields in user admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Registered models here.
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):    # making password non editable
+    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
