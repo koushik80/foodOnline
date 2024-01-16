77fc988ef223b0304ad05618794f88228ed9265b
Koushik <koushik.dey@live.com>
2024-01-16 19:22:11 +0200

    :sparkles: Media Files Configuration
    Media Files Configuration

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -129,6 +129,11 @@ STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
 
+
+# Media files configuration
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
