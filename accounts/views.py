7fc8cc008a181478ebfc4a062714118988fd2215
Koushik <koushik.dey@live.com>
2024-01-17 01:50:20 +0200

    :white_check_mark: Tested User registration path
    Tested User registration path

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+# Created views here
+
+def registerUser(request):
+    return HttpResponse('This is a user registration form')
