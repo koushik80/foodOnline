4fbf6cac539329f3bdad920361ea4d5fe1f5d03d
Koushik <koushik.dey@live.com>
2024-02-05 22:05:04 +0200

    :construction: Login page setup
    Login page setup

--- accounts/views.py
+++ accounts/views.py
@@ -75,3 +75,12 @@ def registerVendor(request):
     }
 
     return render(request, 'accounts/registerVendor.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
+
+def dashboard(request):
+    return
