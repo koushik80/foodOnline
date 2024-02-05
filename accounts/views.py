4f773cd5dba79ceeacc815667d582c6af1d4d81e
Koushik <koushik.dey@live.com>
2024-02-05 23:39:04 +0200

    :white_check_mark: Login and Logout feature and admin panel markup
    Login and Logout feature and admin panel markup

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
-from django.contrib import messages
+from django.contrib import messages, auth
 
 
 # Created views here
@@ -77,10 +77,25 @@ def registerVendor(request):
     return render(request, 'accounts/registerVendor.html', context)
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is None:
+            auth.login(request, user)
+            messages.success(request, 'You are not logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials! Please try again with correct details.')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 def logout(request):
-    return
+    auth.logout(request)
+    messages.info(request, 'You are logged out.')
+    return redirect('login')
 
 def dashboard(request):
-    return
+    return render(request, 'accounts/dashboard.html')
