0d106c76782297fff514e90d116390f150e50b70
Koushik <koushik.dey@live.com>
2024-02-14 00:01:35 +0200

    :zap: Restrict the user to access unauthorized pages
    Restrict the user to access unauthorized pages

--- accounts/views.py
+++ accounts/views.py
@@ -6,10 +6,24 @@ from .forms import UserForm
 from .models import User, UserProfile
 from django.contrib import messages, auth
 from .utils import detectUser
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from django.core.exceptions import PermissionDenied
 
 
-# Created views here
+# Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
 
 def registerUser(request):
     if request.user.is_authenticated:
@@ -88,15 +102,15 @@ def login(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in!')
         return redirect('myAccount')
-    elif request.method == "POST":
+    elif request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
 
         user = auth.authenticate(email=email, password=password)
 
-        if user is None:
+        if user is not None:
             auth.login(request, user)
-            messages.success(request, 'You are not logged in.')
+            messages.success(request, 'You are logged in.')
             return redirect('myAccount')
         else:
             messages.error(request, 'Invalid credentials! Please try again with correct details.')
@@ -115,9 +129,11 @@ def myAccount(request):
     return redirect(redirectUrl)
 
 @login_required(login_url='login')
+@user_passes_test(check_role_customer)
 def custDashboard(request):
     return render(request, 'accounts/custDashboard.html')
 
 @login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render(request, 'accounts/vendorDashboard.html')
